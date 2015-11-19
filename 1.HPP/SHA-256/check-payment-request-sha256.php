304df87c6e689965440be9419d90574ecaaa31b3
joelzou <joel.zou@adyen.com>
2015-11-19 12:21:07 +0100

    Update check-payment-request-sha256.php
    More generic version to get all the URL parameters for merchant signature check

--- 1.HPP/SHA-256/check-payment-request-sha256.php
+++ 1.HPP/SHA-256/check-payment-request-sha256.php
@@ -4,55 +4,69 @@
  * 
  * Whenever a payment is made, the shopper is redirected either to a default 
  * or custom result page. Parameters are appended to this url to provide 
- * a status and general infoamtion about the payment.  
+ * a status and general information about the payment.  
  *
  *  
- * @link    1.HPP/check-payment-response-sha256.php 
+ * @link	1.HPP/check-payment-response-sha256.php 
  * @author	Created by Adyen - Payments Made Easy
  */
  
 /**
- * The variabele $_GET contains an array including 
- * the following keys when avaialble:
- * 
- * $_GET['authResult']
- * $_GET['merchantReference']
- * $_GET['paymentMethod']
- * $_GET['pspReference']
- * $_GET['shopperLocale']
- * $_GET['skinCode']
- * $_GET['merchantReturnData']
- * 
- * 
- * We recommend you to check the consistency of the URL parameters to esure
- * that the data has not been tampered with. 
- * The merchantSig parameter allows you to check it since it is computed using
- * the URL parameters and the HMAC key, same key you used to make the original
- * payment request on our HPP.
+ * We recommend you to check the consistency of the URL parameters to ensure
+ * that the data has not been tampered with, especially when you show
+ * customer/order specific information on the result page, or make updates
+ * to a back office system based on this data.
+ * For this purpose, an HMAC is added to the query string parameters and can be
+ * used to verify the data. The HMAC key which is set on the skin (used for 
+ * the initial payment request) is also used for the calculation of this result
+ * URL parameter.
+ *
+ * Note:
+ * When special characters are returned in the url, PHP automatically replace
+ * them with underscores such as dots.
+ * In some use cases, additional data is returned in the URL and contain dots
+ * such as additionalData.acquirerReference when using klarna. These special
+ * characters must be taken as it is for the merchant signature calculation. 
  *
  */
 
   $hmacKey = "[Your shared Hmac key]"; 
+  
+  /*
+    Function definition
+  */
 
-  // Retrieve the URL parameters
-  $res_merchantSig = $_GET['merchantSig'];
-  $params = array(
-    "authResult" => $_GET['authResult'],
-    "merchantReference" => $_GET['merchantReference'],
-    "paymentMethod" => $_GET['paymentMethod'],
-    "pspReference" => $_GET['pspReference'],
-    "shopperLocale" => $_GET['shopperLocale'],
-    "skinCode" => $_GET['skinCode'],
-  );
+  // Function to preserve the original special character
+  function fix($source) {
+    $source = preg_replace_callback(
+      '/(^|(?<=&))[^=[&]+/',
+      function($key) {
+        return bin2hex(urldecode($key[0]));
+      },
+      $source
+    );
 
-  // merchantReturnData used for the signature calculation only when available
-  if (!empty($_GET['merchantReturnData']))
-    $params["merchantReturnData"] = $_GET['merchantReturnData'];
+    parse_str($source, $post);
+    return array_combine(array_map('hex2bin', array_keys($post)), $post);
+  }
 
   //  Function to escape character
-  $escapeval = function($val) {
+  function escapeval($val) {
     return str_replace(':','\\:',str_replace('\\','\\\\',$val));
   };
+
+  /*
+    Extract the merchant signature and compare it with the one calculated
+    based on the URL parameters
+  */
+  // Get and store all the URL parameters
+  $params = fix($_SERVER['QUERY_STRING']);
+
+  // Retrieve the merchantSig from the URL parameters
+  $res_merchantSig = $params['merchantSig'];
+
+  // Remove the merchantSig field for the signature calculation
+  unset($params['merchantSig']);
 
   // Sort the array by key using SORT_STRING order
   ksort($params, SORT_STRING);
@@ -65,7 +79,7 @@
   $merchantSig = base64_encode(hash_hmac('sha256',$signData,pack("H*" , $hmacKey),true));
 
   // Compare the calculated signature with the signature from the URL parameters
-  if ($merchantSig == $res_merchantSig)
+  if ($merchantSig === $res_merchantSig)
     print "Correct merchant signature";
   else
     print "Incorrect merchant signature";
