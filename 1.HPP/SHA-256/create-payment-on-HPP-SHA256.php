88fc40887afdd6780dca290801919bae52d35775
Rik ter Beek <rikterbeek@users.noreply.github.com>
2016-10-27 15:37:21 +0200

    Update create-payment-on-HPP-SHA256.php

--- 1.HPP/SHA-256/create-payment-on-HPP-SHA256.php
+++ 1.HPP/SHA-256/create-payment-on-HPP-SHA256.php
@@ -10,9 +10,9 @@
      $sharedSecret:    the shared HMAC key.
      */
     
-    $skinCode        = "[skin code e.g. GBIMwmE4]";
-    $merchantAccount = "[merchant Account e.g. TestCompanyCOM]";
-    $hmacKey         = "[HMAC key e.g. D21EB2ASD44BA234C8A0AF13CF0BCACA3D4727C6162630D712C857124B213270]";
+    $skinCode        = "GBIMwmE4"; // your skinCode
+    $merchantAccount = "TestCompanyCOM"; // your merchantAccount
+    $hmacKey         = "D21EB2ASD44BA234C8A0AF13CF0BCACA3D4727C6162630D712C857124B213270"; // your Hmac Key
     
     
     /*
